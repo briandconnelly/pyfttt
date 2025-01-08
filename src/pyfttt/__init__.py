70bb8d7764feae72ee471b662cb434148251d2cf
Brian Connelly <bdc@bconnelly.net>
2025-01-08 09:35:10 -0800

    Replace hardcoded `__version__`

--- src/pyfttt/__init__.py
+++ src/pyfttt/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-VERSION = (0, 3, 2)
-__version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
+from importlib.metadata import version
+
+__version__ = version("pyfttt")
 __license__ = "BSD"
 
-from pyfttt.sending import *
+from .sending import *
